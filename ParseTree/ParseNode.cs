37aca16864ed51dc8136649b2be64ded5ec636b4
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2025-02-09 15:37:32 +0300

    Upgraded to .Net 8.0

--- ParseTree/ParseNode.cs
+++ ParseTree/ParseNode.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using DependencyParser.Universal;
 using Dictionary.Dictionary;
 
 namespace ParseTree
--- ParseTree/ParseTree.cs
+++ ParseTree/ParseTree.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.IO;
-using DependencyParser.Universal;
 using ParseTree.NodeCondition;
 
 namespace ParseTree
